1a877f622cdd1caf5dd54f8ff16d4f8d59a5e1ef
Lucas-GBP <lucas.bitencourte.pedroso@gmail.com>
2023-06-19 12:54:42 -0300

    cross multiplication

--- src/libs/vectors.c
+++ src/libs/vectors.c
@@ -93,3 +93,39 @@ double vector_2d_dot_mult(vector_2d* a, vector_2d*b){
 int vector_2d_int_dot_mult(vector_2d_int* a, vector_2d_int* b){
     return a->x*b->x + a->y*b->y;
 }
+void vector_3d_cross_mult(vector_3d* a, vector_3d* b){
+    vector_3d v = {
+        .x = a->y*b->z - a->z*b->y,
+        .y = a->z*b->x - a->x*b->z,
+        .z = a->x*b->y-a->y*b->x,
+    };
+
+    *a = v;
+}
+vector_3d* vector_3d_cross_mult_return(vector_3d* a, vector_3d* b){
+    vector_3d* v = malloc(sizeof(vector_3d));
+
+    v->x = a->y*b->z - a->z*b->y;
+    v->y = a->z*b->x - a->x*b->z;
+    v->z = a->x*b->y-a->y*b->x;
+
+    return v;
+}
+vector_3d* vector_2d_cross_mul(vector_2d* a, vector_2d* b){
+    vector_3d* v = malloc(sizeof(vector_3d));
+    
+    v->x = 0;
+    v->y = 0;
+    v->z = a->x*b->y-a->y*b->x;
+
+    return v;
+}
+vector_3d_int* vector_2d_int_cross_mult(vector_2d_int* a, vector_2d_int* b){
+    vector_3d_int* v = malloc(sizeof(vector_3d_int));
+
+    v->x = 0;
+    v->y = 0;
+    v->z = a->x*b->y-a->y*b->x;
+
+    return v;
+}
--- src/libs/vectors.h
+++ src/libs/vectors.h
@@ -30,23 +30,32 @@ struct matrix_2x2{
     double element[4];
 };
 
+// Add
 void vector_2d_add(vector_2d* a, vector_2d* b);
 void vector_2d_int_add(vector_2d_int* a, vector_2d_int* b);
 vector_2d* vector_2d_add_return(vector_2d* a, vector_2d* b);
 vector_2d_int* vector_2d_int_add_return(vector_2d_int* a, vector_2d_int* b);
+// Sub
 void vector_2d_sub(vector_2d* a, vector_2d* b);
 void vector_2d_int_sub(vector_2d_int* a, vector_2d_int* b);
 vector_2d* vector_2d_sub_return(vector_2d* a, vector_2d* b);
 vector_2d_int* vector_2d_int_sub_return(vector_2d_int* a, vector_2d_int* b);
+// Mult
 void vector_2d_mult(vector_2d* a, double b);
 void vector_2d_int_mult(vector_2d_int* a, int b);
 vector_2d* vector_2d_mult_return(vector_2d* a, double b);
 vector_2d_int* vector_2d_int_mult_return(vector_2d_int* a, int b);
+// Normilize
 void vector_2d_normilize(vector_2d* v);
 vector_2d* vector_2d_normilize_return(vector_2d* v);
 vector_2d* vector_2d_int_normilize_return(vector_2d_int* v);
+// Dot product
 double vector_2d_dot_mult(vector_2d* a, vector_2d* b);
 int vector_2d_int_dot_mult(vector_2d_int* a, vector_2d_int* b);
-
+// Cross product
+void vector_3d_cross_mult(vector_3d* a, vector_3d* b);
+vector_3d* vector_3d_cross_mult_return(vector_3d* a, vector_3d* b);
+vector_3d* vector_2d_cross_mul(vector_2d* a, vector_2d* b);
+vector_3d_int* vector_2d_int_cross_mult(vector_2d_int* a, vector_2d_int* b);
 
 #endif

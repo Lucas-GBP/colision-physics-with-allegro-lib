ea0c48067c1ba997df041146d772b1bc6c3eb6b7
Lucas-GBP <lucas.bitencourte.pedroso@gmail.com>
2023-06-25 17:04:01 -0300

    refactor

--- src/libs/render.h
+++ src/libs/render.h
@@ -4,6 +4,8 @@
 #include <allegro5\allegro_primitives.h>
 #include "physics.h"
 
+#define FPS_SAMPLE 5
+
 void draw_object(solid_object* object, ALLEGRO_COLOR color);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,81 +1,128 @@
 #include "main.h"
 
-#define QUANT 5000
+#define SOLID_OBJ_QUANT 1000
 
 int main(){
     inicialization();
 
+    // Allegro Variables
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
     ALLEGRO_DISPLAY* disp = al_create_display(1000, 700);
     ALLEGRO_FONT* font = al_create_builtin_font();
-
-    al_register_event_source(queue, al_get_keyboard_event_source());
-    al_register_event_source(queue, al_get_display_event_source(disp));
-    al_register_event_source(queue, al_get_timer_event_source(timer));
-
-    bool redraw = true;
     ALLEGRO_EVENT event;
 
-    solid_object* objects[QUANT];
-    for(uint64_t i = 0; i < QUANT; i++){
+    //Allegro - registering event sources
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_mouse_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    
+    //User input variables
+    bool pressed_keys[ALLEGRO_KEY_MAX];
+    bool pressed_mouse[3];
+    ALLEGRO_MOUSE_EVENT mouse;
+    mouse.x = 0;
+    mouse.y = 0;
+
+    // Alocanting physical objects
+    solid_object* objects[SOLID_OBJ_QUANT];
+    for(uint64_t i = 0; i < SOLID_OBJ_QUANT; i++){
         objects[i] = random_object(WIDTH, HEIGHT, Circle);
     }
 
-    solid_object obj = {
-        .position = {WIDTH/2-50,HEIGHT/2-50},
-        .velocity = {0,0},
-        .axie_velocity = 0,
-        .colision_reaction = NULL,
-        .mass = 1,
-        .shape_type = Circle,
-        .circle = {
-            .radius = 50
-        }
-    };
-
+    // Loop variables
     al_start_timer(timer);
     double old_time = al_get_time();
     int fps_counter = 0;
     double fps = 0;
-    while(true){
+    bool redraw = true;
+    bool loop = true;
+
+    while(loop){
+        // Waiting for any event to happen (timer, keyboard, mouse, ext...)
         al_wait_for_event(queue, &event);
 
-        if(event.type == ALLEGRO_EVENT_TIMER){
+        // Processing diferent event types
+        switch (event.type){
+        case ALLEGRO_EVENT_DISPLAY_CLOSE:
+            loop = false;
+            break;
+        case ALLEGRO_EVENT_KEY_DOWN:
+            pressed_keys[event.keyboard.keycode] = true;
+            break;
+        case ALLEGRO_EVENT_KEY_UP:
+            pressed_keys[event.keyboard.keycode] = false;
+            break;
+        case ALLEGRO_EVENT_MOUSE_AXES:
+            mouse = event.mouse;
+            break;
+        case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+            mouse = event.mouse;
+            if(event.mouse.button < 3)
+                pressed_mouse[event.mouse.button] = true;
+            break;
+        case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+            mouse = event.mouse;
+            if(event.mouse.button < 3)
+                pressed_mouse[event.mouse.button] = false;
+            break;
+        case ALLEGRO_EVENT_TIMER:
             redraw = true;
-        } else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)){
             break;
         }
 
+        // Executed each time event when as the last event
         if(redraw && al_is_event_queue_empty(queue)){
+            // Clean buffer
             al_clear_to_color(al_map_rgb(0, 0, 0));
 
-            draw_object(&obj, al_map_rgb(255, 255, 255));
-            update_physics(objects, QUANT, WIDTH, HEIGHT);
-            for(int i = 0; i < QUANT; i++){
+            // Update all physical bodies
+            update_physics(objects, SOLID_OBJ_QUANT, WIDTH, HEIGHT);
+
+            //
+            //Draw all elements
+            //
+
+            // Physics elements
+            for(int i = 0; i < SOLID_OBJ_QUANT; i++){
                 draw_object(objects[i], al_map_rgb(0, 255, 255));
             }
-
+            //FPS
             al_draw_textf(font, al_map_rgb(255, 255, 255), 5, 5, ALLEGRO_ALIGN_LEFT, "%lf", fps);
+            // Mouse position
+            al_draw_textf(
+                font, 
+                al_map_rgb(255, 255, 255), 
+                (float)(mouse.x+20), 
+                (float)(mouse.y+20), 
+                ALLEGRO_ALIGN_LEFT,
+                "%i  %i",
+                mouse.x, mouse.y
+            );
+
+            //Draw buffer on the screen
             al_flip_display();
 
-            //FPS Count
+            //FPS calculation
             fps_counter++;
-            if(fps_counter >= 5){
+            if(fps_counter >= FPS_SAMPLE){
                 double new_time = al_get_time();
-                fps = fps_counter/((new_time - old_time));
+                fps = fps_counter/(new_time - old_time);
                 old_time = new_time;
                 fps_counter = 0;
             }
 
+            //restar counting
             redraw = false;
         }
     }
 
-    for(int i = 0; i < QUANT; i++){
+    for(int i = 0; i < SOLID_OBJ_QUANT; i++){
         free(objects[i]);
     }
 
+    //Destroing allegro events
     al_destroy_font(font);
     al_destroy_display(disp);
     al_destroy_timer(timer);
@@ -88,5 +135,8 @@ void inicialization(){
     srand((unsigned int)time(NULL));
     al_init();
     al_install_keyboard();
+    al_install_mouse();
     al_init_primitives_addon();
+
+    return;
 }
